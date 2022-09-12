66723e7db9f4449538fc923e563c45fcd3289c47
John Kibirige <kibirigejohn005@gmail.com>
2022-09-12 17:34:47 +0300

    Setup the required components

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import { Header } from './components/Header';
+import { InputTodo } from './components/InputTodo';
+import { TodoList } from './components/TodoList';
+import { NavBar } from './components/NavBar';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [state, setState] = useState([
+    {
+      id: 1,
+      title: 'Setup development environment',
+      completed: true,
+    },
+    {
+      id: 2,
+      title: 'Develop website and add content',
+      completed: false,
+    },
+    {
+      id: 3,
+      title: 'Deploy to live server',
+      completed: false,
+    },
+  ]);
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className='App'>
+      <Header />
+      <InputTodo />
+      <TodoList />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/InputTodo.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const InputTodo = () => {
+  return <div>InputTodo</div>;
+};
